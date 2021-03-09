931aeb96e550286cbe87a8b9e538d37920c68f61
jaykaytherobot <johannekarisolmundarson@gmail.com>
2021-03-09 17:54:21 +0000

    added definition of user table

--- sql/users.sql
+++ sql/users.sql
@@ -1,1 +1,9 @@
-#users_table 
+-- Users tafla --
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users(
+  id serial primary key, 
+  name varchar(128) not null unique, 
+  password varchar(128) not null,
+  admin boolean not null default false
+);
