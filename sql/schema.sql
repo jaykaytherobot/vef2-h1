342f0980c5c94af5647e0e2cdea533afb2b46098
Tryggvi Freyr Sigurgeirsson <57729908+TryggviF@users.noreply.github.com>
2021-03-09 18:43:47 +0000

    Schema fixed, starting steps of setup.js

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,65 +1,65 @@
-CREATE TABLE IF NOT EXISTS Shows {
+CREATE TABLE IF NOT EXISTS Shows(
     id serial PRIMARY KEY,
     name varchar(128) NOT NULL,
     airDate timestamp NOT NULL,
     inProduction boolean, 
     tagline varchar(128),
     img varchar(256) NOT NULL,
-    desc text,
+    description text,
     lang varchar(5),
     network varchar(128) NOT NULL,
     website varchar(256)
-};
+);
 
-CREATE TABLE IF NOT EXISTS Genre {
+CREATE TABLE IF NOT EXISTS Genre(
     id serial PRIMARY KEY,
     name varchar(128) NOT NULL
-};
+);
 
-CREATE TABLE IF NOT EXISTS ShowToGenre {
+CREATE TABLE IF NOT EXISTS ShowToGenre(
     id serial PRIMARY KEY,
-    showID varchar(128),
-    genreID varchar(128),
+    showID serial,
+    genreID serial,
     FOREIGN KEY (showID) REFERENCES Shows(id),
     FOREIGN KEY (genreID) REFERENCES Genre(id)
-};
+);
 
-CREATE TABLE IF NOT EXISTS Season {
+CREATE TABLE IF NOT EXISTS Season(
     id serial PRIMARY KEY,
-    showID varchar(128),
+    showID serial,
     name varchar(128) NOT NULL,
     num integer,
-    CHECK(number > 0),
+    CHECK(num > 0),
     airDate timestamp, 
-    desc text,
+    description text,
     poster varchar(256) NOT NULL,
     FOREIGN KEY (showID) REFERENCES Shows(id)
-};
+);
 
-CREATE TABLE IF NOT EXISTS Episodes {
+CREATE TABLE IF NOT EXISTS Episodes(
     id serial PRIMARY KEY,
-    seasonID varchar(128),
+    seasonID serial,
     name varchar(128) NOT NULL,
     num integer,
     CHECK(num > 0),
     airDate timestamp,
-    desc text,
+    description text,
     FOREIGN KEY (seasonID) REFERENCES Season(id)
-};
+);
 
-CREATE TABLE IF NOT EXISTS Users {
+CREATE TABLE IF NOT EXISTS Users(
     id serial PRIMARY KEY,
     name varchar(128) NOT NULL,
     email varchar(128) NOT NULL,
     password varchar(128) NOT NULL,
     admin boolean DEFAULT FALSE
-};
+);
 
-CREATE TABLE IF NOT EXISTS EpisodeToUser {
-    episodeID varchar(128),
-    userID varchar(128),
+CREATE TABLE IF NOT EXISTS EpisodeToUser(
+    episodeID serial,
+    userID serial,
     status varchar(128),
     grade integer,
     FOREIGN KEY (episodeID) REFERENCES Episodes(id),
     FOREIGN KEY (userID) REFERENCES Users(id)
-};
+);
--- src/setup.js
+++ src/setup.js
@@ -1,10 +1,36 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const {
+  DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = '',
+} = process.env;
+
+console.info('process.env :>> ', process.env.DATABASE_URL);
+
+if (!connectionString) {
+  console.error('Vantar DATABASE_URL!');
+  process.exit(1);
+}
+
+// Notum SSL tengingu við gagnagrunn ef við erum *ekki* í development mode, þ.e.a.s. á local vél
+const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
+
+const pool = new pg.Pool({ connectionString, ssl });
 
 async function setup() {
   // TODO   1. búa til user töflu
   //        2. búa til 1 admin og 1 almennan notenda
   //        3. Búa til þátta töflu
   //        4. Lesa úr .csv skrám til að populate-a
+  const createTable = await readFile('./sql/schema.sql');
+  const tData = createTable.toString('utf-8');
+  result = await query(tData);
+
+  const Seasons = await readFile('./data/series.csv');
+  console.log(Seasons);
 }
 
 await setup();
