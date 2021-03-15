de20cf214b7af41fd7abd9ff38b882a50def5613
jaykaytherobot <johannekarisolmundarson@gmail.com>
2021-03-15 16:33:14 +0000

    setup byr til 2 notendur

--- src/userdb.js
+++ src/userdb.js
@@ -1,7 +1,21 @@
 import { query } from './db.js';
 import bcrypt from 'bcrypt';
 
-export async function createUser(user) {
+export async function createUser(user, admin = false) {
+  if (admin) {
+    const q = 'INSERT INTO Users (name, email, password, admin) VALUES ($1, $2, $3, $4) RETURNING id, email';
+
+    try {
+      const result = await query(q, [user.name, user.email, await bcrypt.hash(user.password, 10), true]);
+      if (result.rowCount === 1){
+        return result.rows[0];
+      }
+    }
+    catch (error) {
+      console.error('Error creating user', error);
+    }
+  }
+
   const q = 'INSERT INTO Users (name, email, password) VALUES ($1, $2, $3) RETURNING id, email';
 
   try {
