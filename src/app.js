6053c71504f9fe9d3d20ac54a73a4911783f0e02
Sverrir Arnórsson <sverrirarnors@gmail.com>
2021-03-18 12:18:55 +0000

    refactor á webtree

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { router as tvRouter, getGenres, postGenres } from './tv.js';
 import { router as userRouter } from './users.js';
 import passport, { requireAdminAuthentication } from './login.js';
+import { webtree } from './webtree.js';
 
 dotenv.config();
 
@@ -18,115 +19,7 @@ app.use(express.json());
 app.use(passport.initialize());
 
 app.get('/', (req, res) => {
-  res.json({
-    tv: {
-      series: {
-        href: '/tv',
-        methods: [
-          'GET',
-          'POST',
-        ],
-      },
-      serie: {
-        href: '/tv/{id}',
-        methods: [
-          'GET',
-          'PATCH',
-          'DELETE',
-        ],
-      },
-      rate: {
-        href: '/tv/{id}/rate',
-        methods: [
-          'POST',
-          'PATCH',
-          'DELETE',
-        ],
-      },
-      state: {
-        href: '/tv/{id}/state',
-        methods: [
-          'POST',
-          'PATCH',
-          'DELETE',
-        ],
-      },
-    },
-    seasons: {
-      seasons: {
-        href: '/tv/{id}/season',
-        methods: [
-          'GET',
-          'POST',
-        ],
-      },
-      season: {
-        href: '/tv/{id}/season/{season}',
-        methods: [
-          'GET',
-          'DELETE',
-        ],
-      },
-    },
-    episodes: {
-      episodes: {
-        href: '/tv/{id}/season/{season}/episode',
-        methods: [
-          'POST',
-        ],
-      },
-      episode: {
-        href: '/tv/{id}/season/{season}/episode/{episode}',
-        methods: [
-          'GET',
-          'DELETE',
-        ],
-      },
-    },
-    genres: {
-      genres: {
-        href: '/genres',
-        methods: [
-          'GET',
-          'POST',
-        ],
-      },
-    },
-    users: {
-      users: {
-        href: '/users',
-        methods: [
-          'GET',
-        ],
-      },
-      user: {
-        href: '/users/{id}',
-        methods: [
-          'GET',
-          'PATCH',
-        ],
-      },
-      register: {
-        href: '/users/register',
-        methods: [
-          'POST',
-        ],
-      },
-      login: {
-        href: '/users/login',
-        methods: [
-          'POST',
-        ],
-      },
-      me: {
-        href: '/users/me',
-        methods: [
-          'GET',
-          'PATCH',
-        ],
-      },
-    },
-  });
+  res.json(webtree);
 });
 
 app.use('/users', userRouter);
