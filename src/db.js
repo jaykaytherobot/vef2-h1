d161ac2d78cc46fa010a4f616e6db9f25a8de4fa
jaykaytherobot <johannekarisolmundarson@gmail.com>
2021-03-21 16:39:49 +0000

    fokkaði í db.js

--- src/db.js
+++ src/db.js
@@ -48,14 +48,6 @@ export async function getAllFromTable(table, attr = '*', offset = 0, limit = 10,
   return result.rows;
 }
 
-export async function updateSerieImageById(id, image) {
-  await query('UPDATE Series SET image = $1 WHERE id = $2', [image, id]);
-}
-
-export async function updateSeasonPosterById(id, poster) {
-  await query('UPDATE Seasons SET poster = $1 WHERE id = $2', [poster, id]);
-}
-
 export async function getSerieById(id) {
   const serieQuery = 'SELECT * FROM Series WHERE id = $1;';
   const serieResult = await query(serieQuery, [id]);
@@ -70,7 +62,7 @@ export async function getSerieByIdWithSeasons(id, userId = false) {
   const ratingQuery = 'SELECT COUNT(*) AS ratingcount, AVG(grade) AS rating FROM SerieToUser WHERE serieId = $1;';
   const genreQuery = 'SELECT name FROM SerieToGenre JOIN Genres ON genreId = Genres.id WHERE serieId = $1;';
   const seasonQuery = 'SELECT * FROM Seasons WHERE serieId = $1;';
-
+  
   try {
     const serieResult = await query(serieQuery, [id]);
     const ratingResult = await query(ratingQuery, [id]);
@@ -176,8 +168,8 @@ export async function createNewSerie(serie) {
       serie.language,
       serie.network,
       serie.homepage]);
-  } else {
-    s = await query('INSERT INTO Series(name, airDate, inProduction, tagline, image, description, language, network, url) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *;',
+    } else {
+      s = await query('INSERT INTO Series(name, airDate, inProduction, tagline, image, description, language, network, url) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *;',
       [serie.name,
         serie.airDate,
         serie.inProduction,
@@ -187,18 +179,18 @@ export async function createNewSerie(serie) {
         serie.language,
         serie.network,
         serie.homepage]);
-  }
-  if (serie.genres) {
-    serie.genres.split(',').forEach(async (genre) => {
-      let genreId;
-      let result;
-      try {
-        await query('INSERT INTO Genres(name) VALUES ($1) ON CONFLICT(name) DO NOTHING;', [genre]);
-        result = await query('SELECT id FROM Genres WHERE name = $1;', [genre]);
-        genreId = result.rows[0].id;
-        await query('INSERT INTO SerieToGenre(serieId, genreId) VALUES ($1, $2);', [serie.id, genreId]);
-      } catch (e) {
-        console.info('Error occured :>> ', e);
+      }
+      if (serie.genres) {
+        serie.genres.split(',').forEach(async (genre) => {
+          let genreId;
+          let result;
+          try {
+            await query('INSERT INTO Genres(name) VALUES ($1) ON CONFLICT(name) DO NOTHING;', [genre]);
+            result = await query('SELECT id FROM Genres WHERE name = $1;', [genre]);
+            genreId = result.rows[0].id;
+            await query('INSERT INTO SerieToGenre(serieId, genreId) VALUES ($1, $2);', [serie.id, genreId]);
+          } catch (e) {
+            console.info('Error occured :>> ', e);
       }
     });
   }
@@ -211,14 +203,14 @@ export async function createNewSeason(season) {
   try {
     result = await query(`INSERT INTO Seasons(serieId, name, "number", airDate, overview, poster)
                               VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`,
-    [season.serieId,
-      season.name,
+                              [season.serieId,
+                                season.name,
       season.number,
       airDate,
       season.overview,
       season.poster]);
-    return result.rows[0];
-  } catch (e) {
+      return result.rows[0];
+    } catch (e) {
     console.info('Error occured :>> ', e);
   }
   return null;
@@ -226,7 +218,7 @@ export async function createNewSeason(season) {
 
 export async function createNewEpisode(episode) {
   const result = await query(`INSERT INTO Episodes(serieId, seasonnumber, name, "number", airdate, overview)
-                              VALUES ($1,$2,$3,$4,$5,$6);`,
+  VALUES ($1,$2,$3,$4,$5,$6);`,
   [episode.serieId,
     episode.season,
     episode.name,
@@ -238,17 +230,8 @@ export async function createNewEpisode(episode) {
 
 export async function createNewUser(user) {
   await query(`INSERT INTO Users(name, email, password, admin)
-                              VALUES ($1,$2,$3,$4);`,
+  VALUES ($1,$2,$3,$4);`,
   [user.name, user.email, user.password, user.admin]);
-}
-
-export async function updateUserRatingBySerieId(serieId, userId, grade) {
-  const result = await query('UPDATE SerieToUser SET grade=$1 WHERE serieId=$2 AND userId=$3 RETURNING *',
-    [grade, serieId, userId]);
-  if (result.rowCount === 1) {
-    return result.rows[0];
-  }
-  return false;
 }
 
 export async function createUserRatingBySerieId(serieId, userId, grade) {
@@ -256,12 +239,12 @@ export async function createUserRatingBySerieId(serieId, userId, grade) {
   const existsResult = await query(queryExists, [serieId, userId]);
   if (existsResult.rowCount === 0) {
     const data = await query(`INSERT INTO SerieToUser(serieId, userId, grade)
-                                VALUES($1,$2,$3) RETURNING *;`,
+    VALUES($1,$2,$3) RETURNING *;`,
     [serieId, userId, grade]);
     console.log(data);
     return data.rows[0];
   }
-
+  
   return updateUserRatingBySerieId(serieId, userId, grade);
 }
 
@@ -270,44 +253,22 @@ export async function createUserStatusBySerieId(serieId, userId, status) {
   const existsResult = await query(queryExists, [serieId, userId]);
   if (existsResult.rowCount === 0) {
     const data = await query(`INSERT INTO SerieToUser(serieId, userId, status)
-                                VALUES($1,$2,$3) RETURNING *;`,
+    VALUES($1,$2,$3) RETURNING *;`,
     [serieId, userId, status]);
     return data.rows[0];
   }
-
+  
   return updateUserStatusBySerieId(serieId, userId, status);
 }
 
-export async function updateUserStatusBySerieId(serieId, userId, status) {
-  const result = await query('UPDATE SerieToUser SET status=$1 WHERE serieId=$2 AND userId=$3 RETURNING *',
-    [status, serieId, userId]);
+
+export async function updateUserRatingBySerieId(serieId, userId, grade) {
+  const result = await query('UPDATE SerieToUser SET grade=$1 WHERE serieId=$2 AND userId=$3 RETURNING *',
+  [grade, serieId, userId]);
   if (result.rowCount === 1) {
     return result.rows[0];
   }
   return false;
-}
-
-export async function deleteUserData(serieId, userId) {
-  const q = 'DELETE FROM SerieToUser WHERE serieId = $1 AND userId = $2 RETURNING *';
-  const result = await query(q, [serieId, userId]);
-  return result.rows;
-}
-
-export async function deleteSerie(id) {
-  const q = 'DELETE FROM Series WHERE id = $1 RETURNING *';
-  const result = await query(q, [id]);
-  return result.rows;
-}
-
-export async function deleteSeasonBySerieIdAndSeasonNumber(id, number) {
-  const q = 'DELETE FROM Seasons WHERE serieId=$1 AND number=$2';
-  await query(q, [id, number]);
-}
-
-export async function deleteEpisode(episodeNum, serieId, seasonNum) {
-  const q = 'DELETE FROM Episodes WHERE id = $1 AND serieId = $2 AND seasonNumber = $3 RETURNING *;';
-  const result = await query(q, [episodeNum, serieId, seasonNum]);
-  return result;
 }
 
 export async function updateSerieById(id, attributes) {
@@ -345,4 +306,44 @@ export async function updateSerieById(id, attributes) {
     }
   }
   return false;
+}
+
+export async function updateUserStatusBySerieId(serieId, userId, status) {
+  const result = await query('UPDATE SerieToUser SET status=$1 WHERE serieId=$2 AND userId=$3 RETURNING *',
+  [status, serieId, userId]);
+  if (result.rowCount === 1) {
+    return result.rows[0];
+  }
+  return false;
+}
+
+export async function updateSerieImageById(id, image) {
+  await query('UPDATE Series SET image = $1 WHERE id = $2', [image, id]);
+}
+
+export async function updateSeasonPosterById(id, poster) {
+  await query('UPDATE Seasons SET poster = $1 WHERE id = $2', [poster, id]);
+}
+
+export async function deleteUserData(serieId, userId) {
+  const q = 'DELETE FROM SerieToUser WHERE serieId = $1 AND userId = $2 RETURNING *';
+  const result = await query(q, [serieId, userId]);
+  return result.rows;
+}
+
+export async function deleteSerie(id) {
+  const q = 'DELETE FROM Series WHERE id = $1 RETURNING *';
+  const result = await query(q, [id]);
+  return result.rows;
+}
+
+export async function deleteSeasonBySerieIdAndSeasonNumber(id, number) {
+  const q = 'DELETE FROM Seasons WHERE serieId=$1 AND number=$2';
+  await query(q, [id, number]);
+}
+
+export async function deleteEpisode(episodeNum, serieId, seasonNum) {
+  const q = 'DELETE FROM Episodes WHERE id = $1 AND serieId = $2 AND seasonNumber = $3 RETURNING *;';
+  const result = await query(q, [episodeNum, serieId, seasonNum]);
+  return result;
 }
