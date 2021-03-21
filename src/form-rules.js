83b51e1115f90c01ae9e9f212cff554437ac1539
Tryggvi Freyr Sigurgeirsson <57729908+TryggviF@users.noreply.github.com>
2021-03-21 20:57:52 +0000

    rationale for eslint-disable

--- src/form-rules.js
+++ src/form-rules.js
@@ -3,6 +3,8 @@ import {
 } from 'express-validator';
 import { getEpisodeByNo, getSerieById, getSeasonBySerieIdAndSeasonNum } from './db.js';
 import * as userDb from './userDb.js';
+// Þetta disable verður að vera hér svo þessi validation
+// rules haga sér rétt
 /* eslint-disable consistent-return */
 export const serieRules = () => [
   body('name')
